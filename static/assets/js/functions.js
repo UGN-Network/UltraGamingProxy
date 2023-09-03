37a5885c33028f7e03d238b0380a36e9b50162f4
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-09-03 18:37:02 -0400

    bruh

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -4,4 +4,11 @@ function yt ()
     }).then(() => {
         location.href=__uv$config.prefix + __uv$config.encodeUrl('https://www.youtube.com/');
     });
+
+    function google () 
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://google.com/');
+    });
    
