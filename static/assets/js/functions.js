2714b025703bc6d81cb4e730296333117517f04f
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-09-03 18:24:05 -0400

    teste the href

---
+++ static/assets/js/functions.js
@@ -0,0 +1,7 @@
+function yt () 
+
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://www.youtube.com/');
+    });
