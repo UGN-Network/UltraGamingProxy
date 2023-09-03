881f94cc78fbf6ea2fd25fcfd137d857c6127afe
Derpman <immapotato8110@gmail.com>
2023-09-02 21:24:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, "static")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "static", "index.html"));
 });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "loader.html"));
+});
 // ADD MORE IF YOU HAVE MORE PAGES
 
 
