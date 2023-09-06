a51c729964e7e6e8894053ed46f96d54b69e1e14
Cosmic Creator *Official* <96603454+CosmicCreatorOfficial@users.noreply.github.com>
2023-09-05 21:34:23 -0400

    ad

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -1,3 +1,10 @@
+function slope(){
+    window.navigator.serviceWorker.register('/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://derpmandev.github.io/unblocked-games/slope');
+    });
+}
 function yt () 
     window.navigator.serviceWorker.register('/sw.js', {
         scope: __uv$config.prefix
